6da82edcc4c2c6f4d6f97dc78bce554c279dd05b
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-10-07 20:24:31 +0200

    added feature to link twitter account with redbooth account as well as the step defintions

--- features/step_definitions/user_account_steps.rb
+++ features/step_definitions/user_account_steps.rb
@@ -8,6 +8,22 @@ When(/^I go to sign up with Twitter$/) do
 end
 
 Then(/^I should be able to create a user account$/) do
-   expect(User.count).to eq(1)
-   expect(page).to have_content('Signed in with Twitter!')
+  expect(User.count).to eq(1)
+  expect(page).to have_content('Signed in with Twitter!')
 end
+
+Given(/^I have an existing account with twitter with name "(.*?)"$/) do |name|
+  existing_user = User.create!(twitter_name: name, twitter_uid: '123455', twitter_nickname: 'nickname')
+  user = User.find_by_twitter_name(name)
+  expect(user).to_not be_nil
+end
+
+When(/^I go to link account with Redbooth$/) do
+  visit root_path
+  click_link 'Link Redbooth'
+end
+
+Then(/^I should be able to link the account with redbooth$/) do
+  expect(page).to have_content('Linked Redbooth account')
+end
+
--- features/user_account.feature
+++ features/user_account.feature
@@ -7,3 +7,8 @@ Feature: Creating a user account in the application
     Given I am a new user with name "new_user"
     When I go to sign up with Twitter
     Then I should be able to create a user account
+
+  Scenario: Link an existing account with redbooth
+    Given I have an existing account with twitter with name "exising_user"
+    When I go to link account with Redbooth
+    Then I should be able to link the account with redbooth
